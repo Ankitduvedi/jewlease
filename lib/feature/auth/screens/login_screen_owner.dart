c35b87d68ca42786085d13deb4682ffacb29d704
Ankitduvedi <ankitduvedi.03@gmail.com>
2024-08-09 17:54:44 +0530

    login

--- lib/feature/auth/screens/login_screen_owner.dart
+++ lib/feature/auth/screens/login_screen_owner.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_element
+
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -15,14 +17,18 @@ class LoginScreen extends ConsumerStatefulWidget {
 }
 
 class LoginScreenState extends ConsumerState<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscureText = true; // Initially password is obscure
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    log("Login screen");
+    log("Login screen staff");
 
     final screenSize = MediaQuery.of(context).size;
     final double verticalPadding = screenSize.height * 0.02;
     final isLoading = ref.watch(authControllerProvider);
-
     return Scaffold(
       body: SafeArea(
         child: isLoading
@@ -30,78 +36,146 @@ class LoginScreenState extends ConsumerState<LoginScreen> {
             : SingleChildScrollView(
                 child: Padding(
                   padding: EdgeInsets.all(screenSize.width * 0.05),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Image.asset(
-                        'assets/login_robot.png',
-                        width: screenSize.width * 0.8,
-                        height: screenSize.height * 0.5,
-                        fit: BoxFit.cover,
-                      ),
-                      SizedBox(height: verticalPadding),
-                      Text(
-                        'Welcome to Reso!',
-                        style: GoogleFonts.pacifico(
-                          fontSize: 28,
-                          color: Colors.brown[700],
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: verticalPadding / 2),
-                      Text(
-                        'Login as a Owner',
-                        style: GoogleFonts.ptSans(
-                          fontSize: 18,
-                          color: Colors.brown[500],
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: verticalPadding),
-                      ElevatedButton(
-                        onPressed: () {
-                          //handleSignIn(ref);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor:
-                              const Color.fromARGB(255, 225, 114, 94),
-                          minimumSize: const Size(double.infinity, 50),
-                          padding: const EdgeInsets.symmetric(horizontal: 12),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(24),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset(
+                          'assets/login_robot.png',
+                          width: screenSize.width *
+                              1, // Dynamic width for the image
+                          height: screenSize.height * 0.43,
+                        ), // Placeholder for the image
+                        Text(
+                          'Welcome to Reso!',
+                          style: GoogleFonts.pacifico(
+                            fontSize: 28,
+                            color: Colors.brown[700],
                           ),
-                          side: BorderSide(
-                            color: Colors.orange[700]!,
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: verticalPadding / 2),
+                        Text(
+                          'Login as a Staff',
+                          style: GoogleFonts.ptSans(
+                            fontSize: 18,
+                            color: Colors.brown[500],
                           ),
+                          textAlign: TextAlign.center,
                         ),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Image.asset('assets/google_logo2.png',
-                                width: 30, height: 30),
-                            const SizedBox(width: 20),
-                            Text(
-                              'Login with Google',
-                              style: GoogleFonts.ptSans(fontSize: 20),
-                            ),
-                          ],
+                        SizedBox(height: verticalPadding),
+
+                        TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter an email address';
+                            } else if (!RegExp(
+                                    r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$')
+                                .hasMatch(value)) {
+                              return 'Please enter a valid email address';
+                            }
+                            return null; // Return null if the input is valid
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Enter Email',
+                            labelStyle: GoogleFonts.ptSans(),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30.0)),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _emailController,
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
+                        SizedBox(height: verticalPadding),
+                        TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (password) => password!.length < 8
+                              ? 'Password must be at least 8 characters'
+                              : null,
+                          controller: _passwordController,
+                          obscureText:
+                              _obscureText, // Toggle this boolean to show/hide password
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: GoogleFonts.ptSans(),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30.0)),
+                            // Added suffixIcon to toggle password visibility
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                // Change the icon based on the state of _obscureText
+                                _obscureText
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
                               onPressed: () {
-                                context.go('/welcomeScreen');
+                                // Update the state to toggle password visibility
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                });
                               },
-                              child: const Text(
-                                'Go back',
-                                style: TextStyle(color: Colors.black),
-                              ))
-                        ],
-                      )
-                    ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              //ref.read(userProvider.notifier).update((state) => null);
+                              ref
+                                  .read(authControllerProvider.notifier)
+                                  .loginWithEmailAndPassword(
+                                      _emailController.text.toString(),
+                                      _passwordController.text.toString(),
+                                      context);
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: const Color.fromARGB(255, 225, 114,
+                                94), // Icon color taken from the logo
+                            minimumSize:
+                                const Size(double.infinity, 50), // Button size
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 12), // Inner padding
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  24), // Rounded corners to match logo style
+                              side: BorderSide(
+                                  color: const Color.fromARGB(255, 255, 114,
+                                      94)!), // Border color taken from the logo
+                            ),
+                            elevation:
+                                0, // No shadow for a flat design similar to the logo
+                          ),
+                          child: Text(
+                            'Sign In',
+                            style: GoogleFonts.ptSans(
+                                fontSize: 20, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                                onPressed: () {
+                                  context.go('/welcomeScreen');
+                                },
+                                child: const Text(
+                                  'Go back',
+                                  style: TextStyle(color: Colors.black),
+                                ))
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
