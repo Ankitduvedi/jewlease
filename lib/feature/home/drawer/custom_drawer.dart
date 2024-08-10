e781495274366bdd28746c794b3fa8bae31a635c
ANKIT KUMAR DUVEDI <ankitduvedi.03@gmail.com>
2024-08-10 06:18:16 +0530

    Drawer

--- lib/feature/home/drawer/custom_drawer.dart
+++ lib/feature/home/drawer/custom_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CustomDrawer extends StatefulWidget {
@@ -22,14 +23,14 @@ class _CustomDrawerState extends State<CustomDrawer> {
             bottomRight: Radius.circular(10),
             topRight: Radius.circular(10),
           ),
-          color: Color.fromRGBO(20, 20, 20, 1),
+          color: Colors.white,
         ),
         child: Column(
           children: [
             IconButton(
               icon: Icon(
                 _isCollapsed ? Icons.arrow_forward_ios : Icons.arrow_back_ios,
-                color: Colors.white,
+                color: Colors.black,
               ),
               onPressed: () {
                 setState(() {
@@ -42,6 +43,64 @@ class _CustomDrawerState extends State<CustomDrawer> {
               child: ListView(
                 padding: EdgeInsets.zero,
                 children: [
+                  _buildExpansionTile(
+                    icon: Icons.currency_rupee_sharp,
+                    title: 'Rate Updation',
+                    subTiles: [
+                      _buildListTile(CupertinoIcons.list_dash,
+                          'Metal Rate Updation Filter'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Stone Buying Rate'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Stone Selling Rate'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'GST Percentage'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Labour Rate Updation'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Discount Rate Updation'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Style(SP) Rate Updation'),
+                      _buildListTile(CupertinoIcons.list_dash,
+                          'Formula Rate Updation(Style Wastage)'),
+                      _buildListTile(CupertinoIcons.list_dash, 'OMP Fineness'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Allowable Discount'),
+                    ],
+                  ),
+                  _buildExpansionTile(
+                    icon: Icons.pin_drop,
+                    title: 'Configuration and Rules',
+                    subTiles: [
+                      _buildExpansionTile(
+                        icon: Icons.pin_drop,
+                        title: 'Destinations',
+                        subTiles: [
+                          _buildListTile(Icons.home, 'Home'),
+                          _buildListTile(Icons.calendar_today, 'Calendar'),
+                        ],
+                      ),
+                      _buildListTile(CupertinoIcons.list_dash,
+                          'Metal Rate Updation Filter'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Stone Buying Rate'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Stone Selling Rate'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'GST Percentage'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Labour Rate Updation'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Discount Rate Updation'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Style(SP) Rate Updation'),
+                      _buildListTile(CupertinoIcons.list_dash,
+                          'Formula Rate Updation(Style Wastage)'),
+                      _buildListTile(CupertinoIcons.list_dash, 'OMP Fineness'),
+                      _buildListTile(
+                          CupertinoIcons.list_dash, 'Allowable Discount'),
+                    ],
+                  ),
                   _buildListTile(Icons.home, 'Home'),
                   _buildListTile(Icons.calendar_today, 'Calendar'),
                   _buildExpansionTile(
@@ -75,13 +134,14 @@ class _CustomDrawerState extends State<CustomDrawer> {
 
   Widget _buildListTile(IconData icon, String title) {
     return ListTile(
-      leading: Icon(icon, color: Colors.white),
+      leading: Icon(icon, color: Colors.black),
       title: AnimatedOpacity(
         opacity: _isCollapsed ? 0.0 : 1.0,
         duration: const Duration(milliseconds: 150),
         child: Text(
+          maxLines: 2,
           title,
-          style: const TextStyle(color: Colors.white),
+          style: const TextStyle(color: Colors.black),
           overflow: TextOverflow.ellipsis, // Ensures text doesn't overflow
         ),
       ),
@@ -101,13 +161,13 @@ class _CustomDrawerState extends State<CustomDrawer> {
         dividerColor: Colors.grey, // Restore the divider color
       ),
       child: ExpansionTile(
-        leading: Icon(icon, color: Colors.white),
+        leading: Icon(icon, color: Colors.black),
         title: AnimatedOpacity(
           opacity: _isCollapsed ? 0.0 : 1.0,
           duration: const Duration(milliseconds: 150),
           child: Text(
             title,
-            style: const TextStyle(color: Colors.white),
+            style: const TextStyle(color: Colors.black),
             overflow: TextOverflow.ellipsis,
           ),
         ),
@@ -121,21 +181,21 @@ class _CustomDrawerState extends State<CustomDrawer> {
     );
   }
 
-  Widget _buildSubTile(IconData icon, String title) {
-    return Column(
-      children: [
-        ListTile(
-          leading: Icon(icon, color: Colors.white),
-          title: Text(
-            title,
-            style: const TextStyle(color: Colors.white70),
-          ),
-          onTap: () {
-            // Handle sub-tile tap
-          },
-        ),
-        const Divider(color: Colors.grey), // Divider between sub-tiles
-      ],
-    );
-  }
+  // Widget _buildSubTile(IconData icon, String title) {
+  //   return Column(
+  //     children: [
+  //       ListTile(
+  //         leading: Icon(icon, color: Colors.black),
+  //         title: Text(
+  //           title,
+  //           style: const TextStyle(color: Colors.black),
+  //         ),
+  //         onTap: () {
+  //           // Handle sub-tile tap
+  //         },
+  //       ),
+  //       const Divider(color: Colors.grey), // Divider between sub-tiles
+  //     ],
+  //   );
+  // }
 }
