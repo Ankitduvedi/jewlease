23f4731e379e83c16534a50bf9bf916b7134831b
ANKIT KUMAR DUVEDI <ankitduvedi.03@gmail.com>
2024-09-23 01:18:46 +0530

    style

--- lib/core/routes/go_router.dart
+++ lib/core/routes/go_router.dart
@@ -20,6 +20,7 @@ import 'package:jewlease/feature/item_specific/screens/add_metal_variant_screen.
 import 'package:jewlease/feature/item_specific/screens/add_packing_material_item_screen.dart';
 import 'package:jewlease/feature/item_specific/screens/add_set_item_screen.dart';
 import 'package:jewlease/feature/item_specific/screens/add_stone_item_screen.dart';
+import 'package:jewlease/feature/item_specific/screens/add_style_item_screen.dart';
 import 'package:jewlease/feature/item_specific/screens/load_data_of_item_master_gold.dart';
 import 'package:jewlease/feature/item_specific/screens/load_data_of_variant_master_gold.dart';
 import 'package:jewlease/feature/item_specific/screens/master_screen.dart';
@@ -91,6 +92,11 @@ final goRouter = GoRouter(
                 return const MasterScreen();
               },
               routes: [
+                GoRoute(
+                    path: 'addStyleItemScreen',
+                    builder: (context, state) {
+                      return const AddStyleItemScreen();
+                    }),
                 GoRoute(
                     path: 'addMetalItemScreen',
                     builder: (context, state) {
