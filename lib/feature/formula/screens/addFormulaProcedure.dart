e6f2ce456bac6f8152d5454d407950c024c7e20a
arpitvermaofficial <arpitv747@gmail.com>
2024-10-08 19:08:28 +0530

    Excel sheet with zoom and dialog open

--- lib/feature/formula/screens/addFormulaProcedure.dart
+++ lib/feature/formula/screens/addFormulaProcedure.dart
@@ -1,18 +1,17 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:jewlease/feature/item_configuration/controller/item_configuration_controller.dart';
 import 'package:jewlease/feature/item_specific/controller/item_master_and_variant_controller.dart';
-import 'package:jewlease/main.dart';
 import 'package:jewlease/providers/dailog_selection_provider.dart';
 import 'package:jewlease/widgets/app_bar_buttons.dart';
 import 'package:jewlease/widgets/number_input_text_field.dart';
 import 'package:jewlease/widgets/read_only_textfield_widget.dart';
 import 'package:jewlease/widgets/text_field_widget.dart';
 
+import '../../../main.dart';
 import '../../../widgets/search_dailog_widget.dart';
 
 class AddFormulaProcedure extends ConsumerStatefulWidget {
@@ -37,10 +36,13 @@ class AddMetalItemScreenState extends ConsumerState<AddFormulaProcedure> {
   final TextEditingController initial = TextEditingController();
 
   InAppWebViewController? webViewController;
+  FocusNode _focusNode = FocusNode();
+
   @override
   void initState() {
     // TODO: implement initState
-
+    _focusNode.requestFocus();
+    // _initializeWebView(jsonData);
     super.initState();
   }
 
@@ -60,7 +62,34 @@ class AddMetalItemScreenState extends ConsumerState<AddFormulaProcedure> {
     'Parent Form',
     'Item Attribute',
   ];
-  void _showOptionsDialog() {
+
+  final String jsonData =
+      "data is [[1, 2, 3, , , , , , , , , , , , , , , , , ], [4, 5, , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ], [, , , , , , , , , , , , , , , , , , , ]]";
+
+  Future<void> _uploadData() async {
+    await Future.delayed(Duration(seconds: 1)); // Allow time for JS to load
+    final data = await webViewController?.evaluateJavascript(
+      source: "getHandsontableData()",
+    );
+
+    print("data is $data");
+  }
+
+  void _showOptionsDialog(int column) {
+    print("column is $column");
+    String dialogContent;
+    switch (column) {
+      case 0:
+        dialogContent = 'You pressed Alt+O on column 0!';
+        break;
+      case 1:
+        dialogContent = 'You pressed Alt+O on column 1!';
+        break;
+      // Add more cases for other columns as needed
+      default:
+        dialogContent = 'You pressed Alt+O on column $column!';
+    }
+
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -70,7 +99,7 @@ class AddMetalItemScreenState extends ConsumerState<AddFormulaProcedure> {
             child: ListBody(
               children: <Widget>[
                 GestureDetector(
-                  child: Text('Option 1'),
+                  child: Text(dialogContent),
                   onTap: () {
                     _selectOption('Option 1');
                   },
@@ -115,102 +144,121 @@ class AddMetalItemScreenState extends ConsumerState<AddFormulaProcedure> {
     final textFieldvalues = ref.watch(dialogSelectionProvider);
     final dropDownValue = ref.watch(dropDownProvider);
 
-    return Scaffold(
-      persistentFooterButtons: [
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.fromLTRB(50, 20, 50, 20),
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              backgroundColor: const Color.fromARGB(255, 40, 112, 62)),
-          child: !ref.watch(itemConfigurationControllerProvider)
-              ? Text(
-                  selectedContent == 0 ? 'Next' : 'Save',
-                  style: const TextStyle(color: Colors.white),
-                )
-              : const CircularProgressIndicator(
-                  color: Colors.white,
-                ),
-        ),
-      ],
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: false,
-        title: const Text('Formula Procdure Details Master'),
-        actions: [
-          AppBarButtons(
-            ontap: [
-              () {},
-              () {},
-              () {
-                // Reset the provider value to null on refresh
-                ref.watch(masterTypeProvider.notifier).state = [
-                  'Style',
-                  null,
-                  null
-                ];
-              },
-              () {}
+    return RawKeyboardListener(
+        focusNode: _focusNode,
+        onKey: (RawKeyEvent event) {
+          if (event.isKeyPressed(LogicalKeyboardKey.altLeft) &&
+              event.isKeyPressed(LogicalKeyboardKey.keyO)) {
+            webViewController?.evaluateJavascript(source: """
+              if (selectedColumn !== null) {
+                window.flutter_inappwebview.callHandler('openDialog', selectedColumn);
+              }
+            """);
+          }
+        },
+        child: Scaffold(
+          persistentFooterButtons: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.fromLTRB(50, 20, 50, 20),
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  backgroundColor: const Color.fromARGB(255, 40, 112, 62)),
+              child: !ref.watch(itemConfigurationControllerProvider)
+                  ? Text(
+                      selectedContent == 0 ? 'Next' : 'Save',
+                      style: const TextStyle(color: Colors.white),
+                    )
+                  : const CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+            ),
+          ],
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            centerTitle: false,
+            title: const Text('Formula Procdure Details Master'),
+            actions: [
+              AppBarButtons(
+                ontap: [
+                  () {},
+                  () {},
+                  () {
+                    // Reset the provider value to null on refresh
+                    ref.watch(masterTypeProvider.notifier).state = [
+                      'Style',
+                      null,
+                      null
+                    ];
+                  },
+                  () {}
+                ],
+              )
             ],
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(children: [
-          Container(
-            height: 150,
-            decoration: BoxDecoration(
-              color: Colors.white, // Background color of the container
-              borderRadius: BorderRadius.circular(12.0), // Rounded corners
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2), // Shadow color
-                  spreadRadius: 1, // How far the shadow spreads
-                  blurRadius: 8, // Softens the shadow
-                  offset: const Offset(
-                      4, 4), // Moves the shadow horizontally and vertically
-                ),
-              ],
-              border: Border.all(
-                color: const Color.fromARGB(
-                    255, 219, 219, 219), // Outline (border) color
-                width: 2.0, // Border width
-              ),
-            ),
-            child: Padding(
-                padding: const EdgeInsets.all(8.0), child: parentForm()),
           ),
-          Container(
-            height: screenHeight * 0.53,
-            child: InAppWebView(
-              initialUrlRequest: URLRequest(
-                url: WebUri.uri(
-                  Uri.file(File(
-                          "C:\\Users\\ASUS\\StudioProjects\\jewlease\\lib\\test.html")
-                      .absolute
-                      .path),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(children: [
+              Container(
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.white, // Background color of the container
+                  borderRadius: BorderRadius.circular(12.0), // Rounded corners
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2), // Shadow color
+                      spreadRadius: 1, // How far the shadow spreads
+                      blurRadius: 8, // Softens the shadow
+                      offset: const Offset(
+                          4, 4), // Moves the shadow horizontally and vertically
+                    ),
+                  ],
+                  border: Border.all(
+                    color: const Color.fromARGB(
+                        255, 219, 219, 219), // Outline (border) color
+                    width: 2.0, // Border width
+                  ),
                 ),
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0), child: parentForm()),
               ),
-              onWebViewCreated: (controller) {
-                webViewController = controller;
-              },
-              onLoadStop: (controller, url) async {
-                await Future.delayed(Duration(seconds: 1));
-                await webViewController?.evaluateJavascript(source: """
-            window.userId = '123456';
-            console.log('Injected User ID:', window.userId);
-          """);
-              },
-            ),
-          )
-          // Expanded(flex: 1, child: ExcelSheet())
-        ]),
-      ),
-    );
+              // Expanded(child: ExcelSheet())
+              Container(
+                height: screenHeight * 0.53,
+                child: InAppWebView(
+                  initialUrlRequest: URLRequest(
+                      url: WebUri.uri(Uri.file(
+                          "C:/Users/ASUS/StudioProjects/jewlease/lib/test.html"))),
+                  initialOptions: InAppWebViewGroupOptions(
+                    crossPlatform: InAppWebViewOptions(
+                      javaScriptEnabled: true,
+                    ),
+                  ),
+                  onWebViewCreated: (controller) {
+                    webViewController = controller;
+                    controller.addJavaScriptHandler(
+                      handlerName: 'openDialog',
+                      callback: (args) {
+                        print("arg is $args");
+                        int column = args[0];
+                        _showOptionsDialog(column);
+                      },
+                    );
+                    // Initialize Handsontable after a short delay
+                    Future.delayed(Duration(seconds: 1), () {
+                      controller.evaluateJavascript(
+                          source: 'initializeHandsontable();');
+                    });
+                  },
+                ),
+              )
+              // Expanded(flex: 1, child: ExcelSheet())
+            ]),
+          ),
+        ));
   }
 
   Widget parentForm() {
@@ -318,7 +366,7 @@ class AddMetalItemScreenState extends ConsumerState<AddFormulaProcedure> {
         TextFieldWidget(labelText: 'Procedure Set', controller: procedureSet),
         IconButton(
             onPressed: () {
-              _showOptionsDialog();
+              _uploadData();
             },
             icon: Icon(Icons.edit))
       ],
